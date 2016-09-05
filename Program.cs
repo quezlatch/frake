5b7093dbacc123e60ac66f591af52a7f62217655
pauldambra <pauldambra@gmail.com>
2016-09-05 23:01:28 +0100

    add a dotnet core project and rake build script

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            File.WriteAllText(
+                "./server.pid", 
+                Process.GetCurrentProcess().Id.ToString());
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {            
+            app.Run(async (context) =>
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                await response.WriteAsync(
+                    "{ \"status\": true, \"statusDate\":  \"" + DateTime.Now.ToString() + "\"}"
+                    );
+                                                               
+            });
+        }
+    }
+}
---
+++ Rakefile
@@ -0,0 +1,47 @@
+require 'net/http'
+
+SERVER_PID = './server.pid'
+
+def kill_process(pid)
+    Process.kill("INT", pid.to_i)
+end
+
+def remove_pid_file
+    File.delete(SERVER_PID) if File.exist? SERVER_PID
+end
+
+task :stop_server do
+    return unless File.exist? SERVER_PID
+    pid = File.read SERVER_PID
+    kill_process pid
+    remove_pid_file
+end
+
+task :clean do
+    FileUtils.rm_rf('bin')
+    FileUtils.rm_rf('obj')
+    remove_pid_file
+end
+
+task :restore do
+    sh "dotnet restore"
+end
+
+task :start do
+    pid = Process.spawn('dotnet', 'run')
+    Process.detach pid
+end
+
+task :ping do
+    begin
+        uri = URI('http://localhost:5000')
+        res = Net::HTTP.get_response(uri)
+        fail "request failed with code #{res.code}" unless res.code == '200'
+        fail 'api is not ready' unless res.body.include? '"status": true'
+        puts 'API IS RUNNING'
+    rescue Exception => e 
+        fail "api request failed ---> #{e.message}"
+    end
+end
+
+task default: [:clean, :restore, :start]
