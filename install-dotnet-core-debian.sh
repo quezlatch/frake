84cd10d6b30972f022506749cf364b11f78e189f
pauldambra <pauldambra@gmail.com>
2016-09-05 23:14:28 +0100

    try to install dotnet core on travis build agent

---
+++ install-dotnet-core-debian.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo apt-get install curl libunwind8 gettext
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?LinkID=809130
+sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
